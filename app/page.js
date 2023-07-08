7815ad0c6433ac3c4b5bbadcf20f775a9d34c452
unknown <aliburakcan1@gmail.com>
2023-07-08 11:46:02 +0300

    pagination

--- app/page.js
+++ app/page.js
@@ -3,43 +3,70 @@
 import React, { useState } from 'react';  
 import axios from 'axios';  
 import SearchBar from '../components/SearchBar';  
-import VideoList from '../components/VideoList'; 
+import VideoList from '../components/VideoList';  
+import Pagination from '../components/Pagination';  
   
 const Home = () => {  
   const [videos, setVideos] = useState([]);  
   const [searchActive, setSearchActive] = useState(false);  
+  const [currentPage, setCurrentPage] = useState(1);  
+  const [totalVideos, setTotalVideos] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');    
+  const videosPerPage = 12;  
   
-  const searchVideos = async (searchTerm) => { 
-    const port = process.env.NEXT_PUBLIC_BACKEND_PORT;
-    const host = process.env.NEXT_PUBLIC_BACKEND_HOST;
-    const http = process.env.NEXT_PUBLIC_BACKEND_HTTP;
-    const url = `${http}://${host}:${port}/api/videos`;
-    console.log(`url: ${url}`);
+  const searchVideos = async (searchTerm, page = 1) => {  
+    const port = process.env.NEXT_PUBLIC_BACKEND_PORT;  
+    const host = process.env.NEXT_PUBLIC_BACKEND_HOST;  
+    const http = process.env.NEXT_PUBLIC_BACKEND_HTTP;  
+    const url = `${http}://${host}:${port}/api/videos`;  
+    console.log(`url: ${url}`);  
     const response = await axios.get(url, {  
-      params: { query: searchTerm },  
+      params: { query: searchTerm, page, limit: videosPerPage },  
     });  
-  
-    setVideos(response.data);  
-    setSearchActive(true);   
+    setVideos(response.data.videos);  
+    setTotalVideos(response.data.total);  
+    setSearchActive(true);  
+    setCurrentPage(page);  
+    setSearchTerm(searchTerm); // Add this line to update the searchTerm state  
   };  
+  
+  const handlePageChange = (newPage, searchTerm) => {    
+    searchVideos(searchTerm, newPage);    
+};   
+    
   
   return (  
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">  
       <div className="container mx-auto px-4">  
         <div className="text-center mb-8">  
-        <img  
+          <img  
             src="tappki_logo1.png"  
             alt="Logo"  
             className="mx-auto mb-4 h-32"  
-          />
+          />  
         </div>  
-        <div className={`w-full ${searchActive ? "mb-8" : "mt-16"}`}>  
+        <div className={`w-full ${searchActive ? "mb-8" : "mt-16"} sticky top-1 z-50`}>  
           <SearchBar onSubmit={searchVideos} />  
         </div>  
-        {videos.length > 0 && <VideoList videos={videos} />}  
+        {videos.length > 0 && (  
+          <VideoList  
+            videos={videos}  
+            key={JSON.stringify(videos)} // Add key prop to force a re-render  
+          />  
+        )}  
+        {totalVideos > 0 && (  
+          <Pagination    
+          totalVideos={totalVideos}    
+          videosPerPage={videosPerPage}    
+          currentPage={currentPage}    
+          onPageChange={handlePageChange}  
+          searchTerm={searchTerm}  
+        />           
+         
+        )}  
       </div>  
     </div>  
-  );   
+  );  
 };  
   
 export default Home;  
---
+++ components/Pagination.js
@@ -0,0 +1,57 @@
+//components/Pagination.js
+
+import React from 'react';  
+
+const Pagination = ({ totalVideos, videosPerPage, currentPage, onPageChange, searchTerm }) => {  
+  const totalPages = Math.ceil(totalVideos / videosPerPage);  
+  
+  const handlePageClick = (page) => {    
+    onPageChange(page, searchTerm);    
+};  
+ 
+   
+  
+  const renderPages = () => {  
+    const pages = [];  
+    for (let i = 1; i <= totalPages; i++) {  
+      pages.push(  
+        <button  
+          key={i}  
+          onClick={() => handlePageClick(i)}  
+          className={`mx-1 my-1 py-2 px-4 rounded bg-white text-gray-800 border border-gray-400 ${  
+            currentPage === i ? 'font-bold' : ''  
+          }`}  
+        >  
+          {i}  
+        </button>  
+      );  
+    }  
+    return pages;  
+  };  
+  
+  return (  
+    <div className="flex justify-center my-8">  
+      <div className="flex flex-wrap justify-center">  
+        {currentPage > 1 && (  
+          <button  
+            onClick={() => handlePageClick(currentPage - 1)}  
+            className="mx-1 my-1 py-2 px-4 rounded bg-white text-gray-800 border border-gray-400"  
+          >  
+            Prev  
+          </button>  
+        )}  
+        {renderPages()}  
+        {currentPage < totalPages && (  
+          <button  
+            onClick={() => handlePageClick(currentPage + 1)}  
+            className="mx-1 my-1 py-2 px-4 rounded bg-white text-gray-800 border border-gray-400"  
+          >  
+            Next  
+          </button>  
+        )}  
+      </div>  
+    </div>  
+  );  
+};  
+  
+export default Pagination;  
