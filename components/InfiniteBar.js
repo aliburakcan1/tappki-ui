269d8095f303de927182a1a279debd70efa32187
aliburakcan1 <aliburakcan1@gmail.com>
2023-12-21 00:51:25 +0300

    make infinite bar clickable

--- components/InfiniteBar.js
+++ components/InfiniteBar.js
@@ -6,54 +6,72 @@ const InfiniteBar = ({ marqueeItems, onItemClicked }) => {
   return (
     <div class="relative flex overflow-x-hidden">
       <div class="py-12 animate-marquee whitespace-nowrap">
-      {marqueeItems.people && marqueeItems.people.map((item, index) => (
-        <span 
-          key={index} 
-          className="text-2xl mx-4" 
-          onClick={() => onItemClicked(item)}
-        >
-          👤 {item}
-        </span>
-      ))}
+        {marqueeItems.people && marqueeItems.people.map((item, index) => (
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            👤 {item}
+          </button>
+        ))}
         {marqueeItems.sport && marqueeItems.sport.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">⚽ {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            ⚽ {item}
+          </button>
         ))}
         {marqueeItems.tags && marqueeItems.tags.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">#️⃣ {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            #️⃣ {item}
+          </button>
         ))}
         {marqueeItems.program && marqueeItems.program.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">📺 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            📺 {item}
+          </button>
         ))}
         {marqueeItems.music && marqueeItems.music.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">🎵 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            🎵 {item}
+          </button>
         ))}
         {marqueeItems.animal && marqueeItems.animal.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">🐾 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            🐾 {item}
+          </button>
         ))}
       </div>
 
       <div class="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
         {marqueeItems.people && marqueeItems.people.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">👤 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            👤 {item}
+          </button>
         ))}
         {marqueeItems.sport && marqueeItems.sport.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">⚽ {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            ⚽ {item}
+          </button>
         ))}
         {marqueeItems.tags && marqueeItems.tags.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">#️⃣ {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            #️⃣ {item}
+          </button>
         ))}
         {marqueeItems.program && marqueeItems.program.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">📺 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            📺 {item}
+          </button>
         ))}
         {marqueeItems.music && marqueeItems.music.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">🎵 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            🎵 {item}
+          </button>
         ))}
         {marqueeItems.animal && marqueeItems.animal.map((item, index) => (
-          <span key={index} class="text-2xl mx-4">🐾 {item}</span>
+          <button key={index} class="text-2xl mx-4" onClick={() => onItemClicked(item)}>
+            🐾 {item}
+          </button>
         ))}
       </div>
     </div>
   )
 }
 
-export default InfiniteBar
+export default InfiniteBar;
