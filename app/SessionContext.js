7816cb0c0650f778fdc2beb15de6ca59c512c696
aliburakcan1 <aliburakcan1@gmail.com>
2023-12-23 15:44:24 +0300

    session id bug fix

--- app/SessionContext.js
+++ app/SessionContext.js
@@ -1,15 +1,31 @@
 // app/SessionContext.js
 
-"use client";
-import React from 'react';  
-import { v4 as uuidv4 } from 'uuid'; // import uuidv4 from uuid package 
+"use client"; 
+import React, { useState, useEffect } from 'react';  
+import { v4 as uuidv4 } from 'uuid'; 
 
-  
 export const SessionContext = React.createContext();  
   
 export const SessionProvider = ({ children }) => {  
-  const sessionId = uuidv4(); // generate UUID here  
-  
+  const [sessionId, setSessionId] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedSessionId = localStorage.getItem('sessionId');
+    if (storedSessionId) {
+      setSessionId(storedSessionId);
+    } else {
+      const newSessionId = uuidv4();
+      localStorage.setItem('sessionId', newSessionId);
+      setSessionId(newSessionId);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or your custom loading component
+  }
+
   return (  
     <SessionContext.Provider value={{ sessionId }}>  
       {children}  
