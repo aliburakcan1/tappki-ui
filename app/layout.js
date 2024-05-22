dd965d4172160cd98dc002774f4b6fdda7979034
aliburakcan1 <aliburakcan1@gmail.com>
2024-05-22 18:05:16 +0300

    monetag ads

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">  
       <head>   
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>  
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4779648569779492" crossorigin="anonymous"></script>  
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4779648569779492" crossorigin="anonymous"></script> 
+        <meta name="monetag" content="16c8ddcbde529956528d0afd72ae1a65"></meta>
       </head>  
       <body>  
         <SessionProvider>  
